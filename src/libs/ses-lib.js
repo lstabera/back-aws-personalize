475cb308ff59d17b7a8edf1ad11a3f1ad8b711d1
lcastro-coronado <luis.castro@condorlabs.io>
2024-01-04 11:05:00 -0500

    add extra changes to template

--- src/libs/ses-lib.js
+++ src/libs/ses-lib.js
@@ -1,15 +1,17 @@
 const { sesService } = require('../services/aws-services');
 
-
-
 const sendEmailP = ({sourceEmail ,receivingEmail, coursesArray}) =>{
     const cursosHTML = coursesArray.map(courses => `
-    <div>
-    <strong>${courses.split('-')[0]}</strong>
-    <ul> 
-      <img src="${courses.split('-')[1]}" width="200"/>
-    </ul>
-  </div>
+    <ul>
+        <li>
+            <img src="${courses.split('-')[1]}" alt="${courses.split('-')[0]}">
+                <div class="course-info">
+                <h2>${courses.split('-')[0]}</h2>
+                <p>Description </p>
+                <a href="#">See more</a>
+            </div>
+        </li>
+        </ul>
     `).join('');
 
      // Create sendEmail params 
@@ -32,35 +34,81 @@ const sendEmailP = ({sourceEmail ,receivingEmail, coursesArray}) =>{
                             <meta http-equiv="X-UA-Compatible" content="IE=edge">
                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
                             <title>¡Discover Our Courses!</title>
-                        </head>
-                        <body style="font-family: Arial, sans-serif;">
-            
-                        <h1 style="color: #3498db;">¡Discover Our Courses!</h1>
-            
-                        <p>Dear ${receivingEmail},</p>
-            
-                        <p>We invite you to explore our exciting selection of courses designed to help you achieve your goals and develop new skills.</p>
-            
-                        <h2>Recommended courses for you:</h2>
-            
-                        <ul>
-                            ${cursosHTML}
-                        </ul>
-            
-                        <p>Don't miss the opportunity to expand your knowledge.</p>
-            
-                        <p>For more information and to register, visit our <a href="https://courses.test.cebroker.com/search/ga/registered-professional-nurse?tab=courses">website</a>.</p>
-            
-                        <p>¡We hope to see you soon!</p>
-            
-                        <p>Sincerely,<br><br>
-                        CEbroker<br>
-                        Propelus</p>
-            
-                        </body>
-                        </html>
-                    `,
-                },
+                            <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                            background-color: #f4f4f4;
+                            margin: 0;
+                            padding: 0;
+                        }
+
+                        h1 {
+                            text-align: center;
+                            color: #333;
+                        }
+
+                        ul {
+                            list-style-type: none;
+                            padding: 0;
+                            margin: 0;
+                            display: flex;
+                            flex-wrap: wrap;
+                            justify-content: center;
+                        }
+
+                        li {
+                            background-color: #fff;
+                            border-radius: 8px;
+                            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                            margin: 10px;
+                            overflow: hidden;
+                            width: 300px;
+                            transition: transform 0.3s ease-in-out;
+                        }
+
+                        li:hover {
+                            transform: scale(1.05);
+                        }
+
+                        img {
+                            width: 100%;
+                            height: auto;
+                            border-bottom: 1px solid #ddd;
+                        }
+
+                        .course-info {
+                            padding: 15px;
+                        }
+                    </style>
+                                        </head>
+                                        <body>
+                            
+                                        <h1 style="color: #3498db;">¡Discover Our Courses!</h1>
+                            
+                                        <p>Dear ${receivingEmail},</p>
+                            
+                                        <p>We invite you to explore our exciting selection of courses designed to help you achieve your goals and develop new skills.</p>
+                            
+                                        <h2>Recommended courses for you:</h2>
+                            
+                                        <ul>
+                                            ${cursosHTML}
+                                        </ul>
+                            
+                                        <p>Don't miss the opportunity to expand your knowledge.</p>
+                            
+                                        <p>For more information and to register, visit our <a href="https://courses.test.cebroker.com/search/ga/registered-professional-nurse?tab=courses">website</a>.</p>
+                            
+                                        <p>¡We hope to see you soon!</p>
+                            
+                                        <p>Sincerely,<br><br>
+                                        CEbroker<br>
+                                        Propelus</p>
+                            
+                                        </body>
+                                        </html>
+                                    `,
+                                },
                 Text: {
                     Charset: "UTF-8",
                     Data: "demo text format body"
